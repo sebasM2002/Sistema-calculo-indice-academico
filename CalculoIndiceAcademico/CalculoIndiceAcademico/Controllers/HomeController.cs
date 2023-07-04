33cef7d5412eac090e21152e0505cbef64962976
sebas <sebastian.mercado1415@gmail.com>
2023-07-04 10:10:19 -0400

    Fix View Problem

--- CalculoIndiceAcademico/CalculoIndiceAcademico/Controllers/HomeController.cs
+++ CalculoIndiceAcademico/CalculoIndiceAcademico/Controllers/HomeController.cs
@@ -8,17 +8,14 @@ namespace CalculoIndiceAcademico.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _context;
 
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
         {
-            _context.Users.ToList();
             return View();
         }
 
