7d4860a14bcc6893a87fe0ef44ce9ebf1c090952
roman <120608459+Estudiante108@users.noreply.github.com>
2023-07-06 11:23:18 -0400

    Add files via upload

---
+++ Controllers/CareerController.cs
@@ -0,0 +1,74 @@
+﻿using CalculoIndiceAcademico.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculoIndiceAcademico.Controllers
+{
+    public class CareerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CareerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Models.CareerModel> objControllerList = _db.Careers;
+            return View(objControllerList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Models.CareerModel obj)
+        {
+            IEnumerable<Models.CareerModel> objControllerList = _db.Careers;
+            foreach (var objController in objControllerList)
+            {
+                if (objController.ID_Career == obj.ID_Career)
+                {
+                    ModelState.AddModelError("ID_Career", $"Clave {objController.ID_Career} has already been used.");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Careers.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET
+        public IActionResult Edit(string id)
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Models.CareerModel obj)
+        {
+         
+            if (ModelState.IsValid)
+            {
+                _db.Careers.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CalculoIndiceAcademico.Data;
+using CalculoIndiceAcademico.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalculoIndiceAcademico.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
